008db1bc418d37e98af0f1644a15c0372f0d61ca
Xavier Lau <c.estlavie@icloud.com>
2018-05-29 11:16:57 +0800

    11:16/05/29/2018

--- usr/themes/wanna/footer.php
+++ usr/themes/wanna/footer.php
@@ -38,8 +38,9 @@
             reload();
         })
     </script>
-    <script src="//s.uvwvu.xyz/live2d/autoload.js" type="text/javascript"></script>
 </footer><!-- end #footer -->
+
+<script src="//s.uvwvu.xyz/live2d/autoload.js" type="text/javascript"></script>
 
 <?php $this->footer(); ?>
         </div>
