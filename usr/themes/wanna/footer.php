a57cc1420b192cc6bf1c192e2326ab4f01fedc34
Xavier Lau <c.estlavie@icloud.com>
2018-05-29 11:45:16 +0800

    11:45/05/29/2018

--- usr/themes/wanna/footer.php
+++ usr/themes/wanna/footer.php
@@ -45,20 +45,6 @@
         </div>
 <?php $this->need('sidebar.php'); ?>
     
-<!-- <div class="waifu">
-    <div class="waifu-tips"></div>
-    <canvas id="live2d" width="280" height="250" class="live2d"></canvas>
-    <div class="waifu-tool">
-        <span class="fui-home"></span>
-        <span class="fui-chat"></span>
-        <span class="fui-eye"></span>
-        <span class="fui-user"></span>
-        <span class="fui-photo"></span>
-        <span class="fui-info-circle"></span>
-        <span class="fui-cross"></span>
-    </div>
-</div> -->
-
 <script src="//s.uvwvu.xyz/live2d/autoload.js" type="text/javascript"></script>
 
     </body>
