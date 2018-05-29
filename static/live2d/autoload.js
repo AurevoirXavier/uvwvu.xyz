d5eedfcc52b739dc326c54a0bad93619262a5e60
Xavier Lau <c.estlavie@icloud.com>
2018-05-29 11:53:11 +0800

    11:53/05/29/2018

--- static/live2d/autoload.js
+++ static/live2d/autoload.js
@@ -2,8 +2,8 @@ $.ajaxSetup({
     cache: true
 });
 
-$.getScript('//s.uvwvu.xyz/live2d/live2d.js').done(function() {
-    $.getScript('//s.uvwvu.xyz/live2d/waifu-tips.js').done(function() {
+$.getScript('//s.uvwvu.xyz/live2d/waifu-tips.js').done(function() {
+    $.getScript('//s.uvwvu.xyz/live2d/live2d.js').done(function() {
         $("<link>").attr({href: "//s.uvwvu.xyz/live2d/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
         $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" width="280" height="250" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
 
