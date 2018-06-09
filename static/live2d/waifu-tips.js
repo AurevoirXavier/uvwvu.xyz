b59c9b1a1d711cc657ff78d2f25f8be37164333c
Xavier Lau <c.estlavie@icloud.com>
2018-06-09 13:19:46 +0800

    13:19/06/09/2018

--- static/live2d/waifu-tips.js
+++ static/live2d/waifu-tips.js
@@ -96,18 +96,18 @@ function waifuWelcome(){
             referrer.href = document.referrer;
             var domain = referrer.hostname.split('.')[1];
             if (window.location.hostname == referrer.hostname) {
-                text = '欢迎阅读<span style="color:orange;">' + document.title.split(' - ')[0] + '</span>';
+                text = '欢迎阅读 <span style="color:red;">' + document.title.split(' - ')[0] + '</span>';
             } else if (domain == 'baidu') {
-                text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:orange;">' + referrer.search.split('&wd=')[1].split('&')[0] + '</span> 找到的我吗？';
+                text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:red;">' + referrer.search.split('&wd=')[1].split('&')[0] + '</span> 找到的我吗？';
             } else if (domain == 'so') {
-                text = 'Hello! 来自 360搜索 的朋友<br>你是搜索 <span style="color:orange;">' + referrer.search.split('&q=')[1].split('&')[0] + '</span> 找到的我吗？';
+                text = 'Hello! 来自 360搜索 的朋友<br>你是搜索 <span style="color:red;">' + referrer.search.split('&q=')[1].split('&')[0] + '</span> 找到的我吗？';
             } else if (domain == 'google') {
-                text = 'Hello! 来自 谷歌搜索 的朋友<br>欢迎阅读<span style="color:orange;">' + document.title.split(' - ')[0] + '</span>';
+                text = 'Hello! 来自 谷歌搜索 的朋友<br>欢迎阅读 <span style="color:red;">' + document.title.split(' - ')[0] + '</span>';
             } else {
-                text = 'Hello! 来自 <span style="color:orange;">' + referrer.hostname + '</span> 的朋友';
+                text = 'Hello! 来自 <span style="color:red;">' + referrer.hostname + '</span> 的朋友';
             }
         } else {
-            text = '欢迎阅读<span style="color:orange;">' + document.title.split(' - ')[0] + '</span>';
+            text = '欢迎阅读 <span style="color:red;">' + document.title.split(' - ')[0] + '</span>';
         }
     }
     showMessage(text, 6000);
