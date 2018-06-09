c2ac51c6f5779d38680e2c78ecbe4b436d921b88
Xavier Lau <c.estlavie@icloud.com>
2018-06-09 11:59:21 +0800

    11:59/06/09/2018

--- usr/themes/wanna/archive.php
+++ usr/themes/wanna/archive.php
@@ -8,33 +8,46 @@
             'author'    =>  _t('%s\'s publish')
         ), '', ''); ?></h3>
     <?php if ($this->have()): ?>
+        <div class="mdui-row">
         <?php while($this->next()): ?>
             <article class="s-data-card mdui-col-md-4">
                 <div class="s-data-card-con shadow-2">
-                    <div class="colorHi"></div>
+                    <div class="colorHi">
+                        <?php if($this->options->slimg && 'guanbi'==$this->options->slimg): ?>
+                        <?php else: ?>
+                            <?php if($this->options->slimg && 'showoff'==$this->options->slimg): ?><a href="<?php $this->permalink() ?>" ><?php showThumbnail($this); ?></a>
+                            <?php else: ?>
+                                <div class="cardImage-img" style="background-image: url('<?php showThumbnail($this); ?>')"></div>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    </div>
                     <h5><?php $this->title() ?></h5>
                     <p>
                         <?php _e('Author: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a>
                         &nbsp<?php _e('Date: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
                     </p>
                     <p>
-                        <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('Comment', '1 条评论', '%d 条评论'); ?></a>
+                        <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('Not comment yet', '1 comment', '%d comments'); ?></a>
                     </p>
                     <div class="mdui-divider" style="margin-bottom: 10px"></div>
                     <p>
-                        <?php $this->excerpt(50,'...'); ?>
+                        <?php $this->excerpt(30,'...'); ?>
                     </p>
                 </div>
             </article>
         <?php endwhile; ?>
+        </div>
     <?php else: ?>
         <div>Welcome to the Void. Try searching?</div>
         <form method="post" class="findMore-404 getData-input">
             <input placeholder="Searching...( Enter )" type="text" name="s" autofocus />
         </form>
     <?php endif; ?>
-
-    <?php $this->pageNav('&laquo;', '&raquo;'); ?>
+    <div class="mdui-row" id="search-list-nav">
+        <div class="nav mdui-col-md-4 mdui-col-xs-10 mdui-col-offset-xs-1">
+            <?php $this->pageNav('<i class="mdui-icon material-icons">keyboard_arrow_left</i>', '<i class="mdui-icon material-icons">&#xe315;</i>',0, '...', 'wrapTag=ul&wrapClass=page-navigator&itemTag=li&textTag=span&tClass=current&prevClass=prev&nextClass=next'); ?>
+        </div>
+    </div>
 </div><!-- end #main -->
 
 <?php $this->need('footer.php'); ?>
