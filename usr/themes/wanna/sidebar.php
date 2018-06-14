b10cebcde358f8390086154c702424e0c2dbecd8
Xavier Lau <c.estlavie@icloud.com>
2018-06-14 12:36:42 +0800

    12:36/06/14/2018

--- usr/themes/wanna/sidebar.php
+++ usr/themes/wanna/sidebar.php
@@ -21,7 +21,7 @@
         </a>
         <a href="https://github.com/aurevoirxavier">
             <li class="mdui-ripple">
-                <i class="fa github"></i>
+                <i class="fa fa-github"></i>
             </li>
         </a>
     </ul>
