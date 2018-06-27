18581797d332fb8ea2f501028a7284f6126633de
Xavier Lau <c.estlavie@icloud.com>
2018-06-27 10:16:37 +0800

    10:16/06/27/2018

--- usr/themes/wanna/page.php
+++ usr/themes/wanna/page.php
@@ -12,7 +12,7 @@
         <h3><?php $this->title() ?></h3>
         <div class="taptap">
             <div>
-                <span>浏览量：<a><?php get_post_view($this) ?></a></span>
+                <span>views：<a><?php get_post_view($this) ?></a></span>
                 <span><?php $this->date('Y / F j'); ?></span>
                 <a><?php $this->author() ?></a></span>
             </div>
@@ -24,5 +24,4 @@
     </div>
 </div>
 
-<?php $this->need('sidebar.php'); ?>
 <?php $this->need('footer.php'); ?>
