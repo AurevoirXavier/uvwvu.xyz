78850c3b67eeb365397a2b5b033c34e81d91ebaf
Xavier Lau <c.estlavie@icloud.com>
2018-06-30 12:56:38 +0800

    12:56/06/30/2018

--- usr/themes/wanna/index.php
+++ usr/themes/wanna/index.php
@@ -37,7 +37,7 @@ $this->need('header.php');
                     <div class="cardContent">
                         <div class="mdui-typo">
                             <div class="pageTitle">
-                                <h4><?php $this->title() ?></h4>
+                                <h4><font color="black"><?php $this->title() ?></font></h4>
                             </div>
                             <div style="margin-bottom: 10px;line-height: 1.5em">
                                 Author: <a><?php $this->author(); ?></a>&nbsp
