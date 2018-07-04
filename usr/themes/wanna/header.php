56f7568d260c7f8e573d70788e0ea18fb7803b5a
Xavier Lau <c.estlavie@icloud.com>
2018-07-04 13:40:58 +0800

    13:40/07/04/2018

--- usr/themes/wanna/header.php
+++ usr/themes/wanna/header.php
@@ -48,10 +48,10 @@
                     </a>
                     <li class="mdui-ripple" id="controlBtn">
                         <button class="mb-btn" id="open-Vmenu">
-                            <i class="material-icons mdui-icon">menu</i>
+                            <i class="material-icons mdui-icon">details</i>
                         </button>
                         <button class="mb-btn mdui-ripple" id="open-hMenu">
-                            <i class="mdui-icon material-icons">details</i>
+                            <i class="mdui-icon material-icons">menu</i>
                         </button>
                     </li>
     </ul>
