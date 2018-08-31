342a008726bbf4d4b7b028fdbb23998121e82ade
Xavier Lau <c.estlavie@icloud.com>
2018-08-31 20:01:42 +0800

    20:01/08/31/2018

--- usr/themes/wanna/comments.php
+++ usr/themes/wanna/comments.php
@@ -50,7 +50,7 @@ if(is_array($_GET)&&count($_GET)>0)
             <form method="post" action="<?php $this->commentUrl() ?>" style="width: 100%" role="form" id="comment_form">
             <div class="replyId" id="replyId" style="float: left">To: <span class="reply-name"><?php echo $this->author->name();?></span>&nbsp&nbsp<span class="replyCon" style="background-color: rgb(235, 235, 235)"></span></div>
                 <?php if($this->user->hasLogin()): ?>
-                    <div style="float: right"><?php _e('From: '); ?><?php $this->user->screenName(); ?>. &nbsp<a href="<?php $this->options->logoutUrl(); ?>" title="Sign out"><?php _e('Sign out'); ?> &raquo; </a></div>
+                    <div style="float: right"><?php _e('From: '); ?><?php $this->user->screenName(); ?>. &nbsp<a style="display: initial" href="<?php $this->options->logoutUrl(); ?>" title="Sign out"><?php _e('Sign out'); ?> &raquo; </a></div>
                 <?php else: ?>
                     <a class="smallSize" href="<?php $this->options->adminUrl(); ?>">Sign in</a>
                     <div class="userIC">
