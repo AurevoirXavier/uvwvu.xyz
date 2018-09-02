ece4e8d02f577b587761f4cb9dc6e16e38e21127
Xavier Lau <c.estlavie@icloud.com>
2018-09-03 03:10:23 +0800

    03:10/09/03/2018

--- usr/themes/wanna/comments.php
+++ usr/themes/wanna/comments.php
@@ -88,7 +88,7 @@ if(is_array($_GET)&&count($_GET)>0)
                 <?php if($tags->have()): ?>
                 <nav class="tags-list">
                     <?php while ($tags->next()):
-                        echo "<a href=" . $tags->permalink() . ">" . $tags->name() . "</a>"; 
+                        echo "<a href=\"" . $tags->permalink() . "\">" . $tags->name() . "</a>"; 
                     endwhile; ?>
                     <?php else: ?>
                         <a><?php _e('No labels yet'); ?></a>
